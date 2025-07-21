0a7c63db73e56054816e867605dee8767b699b6b
Aunnno <2307738723@qq.com>
2025-07-21 22:59:31 +0800

    the plugins config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+require'nvim-treesitter.configs'.setup {
+  -- 添加不同语言
+  ensure_installed = { "vim", "vimdoc", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+
+  highlight = { enable = true },
+  indent = { enable = true },
+
+  -- 不同括号颜色区分
+}

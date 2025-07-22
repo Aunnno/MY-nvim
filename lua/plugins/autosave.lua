462f8a4d74444011a8df3e2e0f2ba23bba296d6a
Aunnno <2307738723@qq.com>
2025-07-22 16:30:52 +0800

    Plugins

---
+++ lua/plugins/autosave.lua
@@ -0,0 +1,4 @@
+require'auto-save'.setup{
+	timeout = 2500,
+	ignore_buffers = {'/dev/null','pr://'}
+}
--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -28,21 +28,41 @@ local plugins = {
 	"akinsho/bufferline.nvim", -- buffer分割线
     "lewis6991/gitsigns.nvim", -- 左则git提示
 	"p00f/nvim-ts-rainbow", -- 配合treesitter，不同括号颜色区分
---	{
---   "williamboman/mason.nvim",
+-- {
+--    "williamboman/mason.nvim",
 --    "williamboman/mason-lspconfig.nvim", -- 这个相当于mason.nvim和lspconfig的桥梁
 --    "neovim/nvim-lspconfig"
 --   },
 
 	     -- 自动补全  已移除，开发板上会崩溃，疑似爆内存
---  "hrsh7th/nvim-cmp",
---  "hrsh7th/cmp-nvim-lsp",
---  "L3MON4D3/LuaSnip", -- snippets引擎，不装这个自动补全会出问题
---  "saadparwaiz1/cmp_luasnip",
---  "rafamadriz/friendly-snippets",
---  "hrsh7th/cmp-path", -- 文件路径
-  "numToStr/Comment.nvim", -- gcc和gc注释
-  "windwp/nvim-autopairs" -- 自动补全括号
+--    "hrsh7th/nvim-cmp",
+--    "hrsh7th/cmp-nvim-lsp",
+--    "L3MON4D3/LuaSnip", -- snippets引擎，不装这个自动补全会出问题
+--    "saadparwaiz1/cmp_luasnip",
+--    "rafamadriz/friendly-snippets",
+--    "hrsh7th/cmp-path", -- 文件路径
+    "numToStr/Comment.nvim", -- gcc和gc注释
+    "windwp/nvim-autopairs", -- 自动补全括号
+-- 	{
+-- 	"iamcco/markdown-preview.nvim",
+--   	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--   	build = "cd app && yarn install",
+--   	init = function()
+--     vim.g.mkdp_filetypes = { "markdown" }
+--   end,
+--   	ft = { "markdown" },
+-- },
+	"Pocco81/auto-save.nvim",
+	{
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+	}
 }
 local opts = {} -- 注意要定义这个变量
 
